e64fa18c5ef275acd4e0ef78bd16952d6c8fed25
sourabhgupta385 <gupta.sourabh385@gmail.com>
2020-03-20 15:39:13 +0530

    added script for triggering jobs

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# jenkins
-It is custom jenkins which already has 20 jobs created and some plugin installed mentioned in plugins file.
+# Custom Jenkins
+
+It is custom jenkins which already has 26 jobs created and plugin installed that are mentioned in plugins file.
+
+## Setup on Docker Environment
+
+Use below command to get jenkins running as docker container:
+
+```
+$ docker run -d --name myjenkins -p 8080:8080 -p 50000:50000 sourabh385/custom-jenkins
+```
+
+## Steps to trigger Jenkins jobs through command line:
+
+1. Specify the value of JENKINSURL, USERNAME, PASSWORD in _build-jobs.sh_ file.
+
+2. Execute the script.
+
+```
+$ sh build-jobs.sh
+```
+
+## Categorization of 26 Jobs
+
+### Successful Jobs
+
+Jobs named A-J will all result as SUCCESS
+
+### Failing Jobs
+
+Jobs named K-T will all result as FAILED
+
+### Unstable Jobs
+
+Jobs named U-Z will all result as UNSTABLE
---
+++ build-jobs.sh
@@ -0,0 +1,31 @@
+#Specify the value of JENKINSURL before running this file.
+JENKINSURL=
+USERNAME=
+PASSWORD=
+
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/A/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/B/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/C/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/D/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/E/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/F/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/G/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/H/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/I/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/J/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/K/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/L/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/M/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/N/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/O/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/P/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/Q/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/R/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/S/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/T/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/U/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/V/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/W/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/X/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/Y/build
+curl -X POST -u${USERNAME}:${PASSWORD} ${JENKINSURL}/job/Z/build
